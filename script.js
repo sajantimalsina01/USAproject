86361a1a23f855b82a88614aa276a6045b339a9f
SajanTinalsina <sajantimalsina01@gmail.com>
2025-05-25 21:53:21 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".info-card");
+
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.style.opacity = 1;
+          entry.target.style.transform = "translateY(0)";
+        }
+      });
+    },
+    {
+      threshold: 0.2
+    }
+  );
+
+  cards.forEach(card => {
+    observer.observe(card);
+  });
+});
